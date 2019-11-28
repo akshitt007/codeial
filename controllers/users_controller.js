43f666404e9056d7c07426cbefa9b4b1d1dd0638
Akshit Arora <akshitarora4731@gmail.com>
2019-11-28 05:47:09 +0530

    rendering pages for signIn and signUp

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,3 +4,14 @@ module.exports.profile =function(req,res){
          title: "Profile page"
     });
 }
+
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title: "Codeial|Sign up"
+    })
+}
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title: "Codeial|Sign In"
+    })
+}
