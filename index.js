2fbbb9f0672a29ad5d6cab2a94cbc9070b979c42
Akshit Arora <akshitarora4731@gmail.com>
2019-11-28 09:27:54 +0530

    create and alter a cookie

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
 const db = require('./config/mongoose');
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
 
 //express layout library 
 const expressLayouts = require('express-ejs-layouts');
