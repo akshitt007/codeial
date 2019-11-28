e260a6879b08a96e6d55acddfa023b0d683ec3ed
Akshit Arora <akshitarora4731@gmail.com>
2019-11-28 05:30:46 +0530

    set up user Schema for mongoDB

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const mongoose = new mongoose.Schema({
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    name:{
+        type: String,
+        required: true,
+    },
+},
+       
+{
+    //for recording time of updates and stuff
+    timestamps:true
+    
+});
+
+const user =mongoose.model('user', userSchema);
+
+module.exports =user;
