aa72f6f89ad58d2495d68e6fe2fffbf11275a442
Akshit Arora <akshitarora4731@gmail.com>
2020-01-08 21:18:59 +0530

    add flash msg gor log in and log out

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -78,11 +78,12 @@ module.exports.create = function(req, res){
 
 // sign in and create a session for the user
 module.exports.createSession = function(req, res){
+    req.flash('success','Log In Successfull');
     return res.redirect('/');
 }
 
 module.exports.destroySession = function(req, res){
     req.logout();
-
+    req.flash('success','you have logged out');
     return res.redirect('/');
 }
